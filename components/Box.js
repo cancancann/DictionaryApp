a5b9cafddfb8c8ab95db677369ca0fb6149c88e8
Hikmet Can Öyke <hikmetcanoyke@outlook.com>
2023-04-01 15:17:14 +0300

    implementing svg

--- components/Box.js
+++ components/Box.js
@@ -3,5 +3,7 @@ import styled from "styled-components";
 import { color, compose, flexbox, size, space } from "styled-system";
 
 const Box = styled(View)(compose(color, size, space, flexbox));
+//kendin ayarlayabiliyorssun compose içine ne tarz style işlemlerini gireceğini belirtip ayarlayabiliyorsun
+//styled içine de ne tarz bir componente bağlı olduğunu belirtmen gerekiyor
 
 export default Box;
--- views/DetailView.js
+++ views/DetailView.js
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
+
+//svg import
+import BookMark from "../icons/bookmark.svg";
+
+
 const DetailView = () => {
   return (
     <View>
       <Text>DetailView</Text>
+      <View>
+        <BookMark  width={50} height={50} fill="none" stroke="black" strokeOpacity="0.7" />
+      </View>
     </View>
   );
 };
